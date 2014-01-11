a70860dead5dc702b5a1ae71cb88fd105a4291c6
Julien Bergner <julien@bergner.fr>
2014-01-11 02:31:21 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AvitallShop::Application.config.secret_key_base = '0351655b167db1e8a398b95ea438c0a5a08cd9798ef7a0b038cdf730a0eaa206f1a2b94ef4454a66865426f72e6a5cd05bae74acbfb2935e61ed5d17d77fc116'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AvitallShop::Application.config.session_store :cookie_store, key: '_avitall-shop_session'
