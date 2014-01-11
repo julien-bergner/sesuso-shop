406cd785838f65534f22b81a13e2c0b327bb637c
Julien Bergner <julien@bergner.fr>
2014-01-11 03:39:57 +0100

    Add uberspacify gem

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+# include uberspacify base recipes
+require 'uberspacify/base'
+
+# comment this if you don't use MySQL
+require 'uberspacify/mysql' 
+
+# the Uberspace server you are on
+server 'hercules.uberspace.de', :web, :app, :db, :primary => true
+
+# your Uberspace username
+set :user, 'avitall'
+
+# a name for your app, [a-z0-9] should be safe, will be used for your gemset,
+# databases, directories, etc.
+set :application, 'AvitallShop'
+
+# the repo where your code is hosted
+set :shell, '/usr/bin/bash'
+set :scm, :git
+set :repository, 'git@github.com:julien-bergner/avitall-shop.git'
+
+# optional stuff from here
+
+# By default, your app will be available in the root of your Uberspace. If you
+# have your own domain set up, you can configure it here
+# set :domain, 'www.sesuso.de'
+
+# By default, uberspacify will generate a random port number for Passenger to
+# listen on. This is fine, since only Apache will use it. Your app will always
+# be available on port 80 and 443 from the outside. However, if you'd like to
+# set this yourself, go ahead.
+# set :passenger_port, 20019
+
+# By default, Ruby Enterprise Edition 1.8.7 is used for Uberspace. If you
+# prefer Ruby 1.9 or any other version, please refer to the RVM documentation
+# at https://rvm.io/integration/capistrano/ and set this variable.
+set :rvm_ruby_string, 'ruby-2.0.0-p353@AvitallShop'
