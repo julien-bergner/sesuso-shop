3d23bc9511987e06b459399833cbbc2a2bf5d53b
Julien Bergner <julien@bergner.fr>
2014-01-15 14:37:48 +0100

    Update precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,9 @@ AvitallShop::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( store/print.css )
+  config.assets.precompile += %w( icons.svg icons.ttf icons.eot icons.woff )
+  config.assets.precompile += %w( images/bx_loader.gif images/controls.png )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
