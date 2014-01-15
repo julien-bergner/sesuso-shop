bd8e3c25b3f05e29f7731eec7472df6f7bd6b19c
Julien Bergner <julien@bergner.fr>
2014-01-15 04:05:07 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140114232911) do
+ActiveRecord::Schema.define(version: 20140115003247) do
 
   create_table "spree_activator_translations", force: true do |t|
     t.integer  "spree_activator_id"
