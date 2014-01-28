a3261978701dbc04ff8e2f9fea5c39dd90b3e4d0
Julien Bergner <julien@bergner.fr>
2014-01-28 21:58:20 +0100

    Update invoice template

--- config/initializers/spree_invoice.rb
+++ config/initializers/spree_invoice.rb
@@ -5,26 +5,15 @@ WickedPdf.config = {
 }
 
 SpreeInvoice.setup do |config|
-  config.on_confirm_email = true
-  config.invoice_number_generation_method = lambda { |next_invoice_count|
-        number = "%04d" % next_invoice_count.to_s
-        "R-#{Time.now.year}-#{number}"
+  config.on_confirm_email = false
+
+  config.invoice_template_path = "app/views/spree/invoices/invoice.html.erb"
+
+  config.wkhtmltopdf_margin = {
+    :top    => 0,
+    :bottom => 0,
+    :left   => 0,
+    :right  => 0
   }
   
-  config.invoice_template_path = "app/views/spree/invoices/invoice_template.html.erb"
-  config.except_payment = ['Spree::PaymentMethod::Check']
-  config.wkhtmltopdf_margin = {
-    :top    => 10,
-    :bottom => 10,
-    :left   => 15,
-    :right  => 15
-  }
-  config.invoice_seller_details = %Q|
-  Spree Demo
-  P.IVA: 12345678901
-  C.F.: 12345678901
-  Street Address, 12
-  00000 City (STATE)
-  |
-  config.invoice_seller_logo = Spree::Config[:admin_interface_logo]
 end
