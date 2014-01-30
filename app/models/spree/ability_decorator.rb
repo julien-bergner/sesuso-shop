0d806320577da103f1802c2438761b27be22dd7a
Julien Bergner <julien@bergner.fr>
2014-01-30 21:02:19 +0100

    Fix typo in ability decorator

--- app/models/spree/ability_decorator.rb
+++ app/models/spree/ability_decorator.rb
@@ -13,7 +13,7 @@ module Spree
         can [:admin, :manage], Shipment
         can [:admin, :manage], ReturnAuthorization
 
-        can [:admin, :index, :show], Reports
+        can [:admin, :show], Report
 
       end
 
