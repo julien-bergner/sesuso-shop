e674f0dbfcfa8d57a83af89a92b515bbf824bd94
Julien Bergner <julien@bergner.fr>
2014-01-31 02:12:58 +0100

    Adapt featured and slider name

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,10 +1,10 @@
 Spree::HomeController.class_eval do
 
   def index
-    slider = Spree::Taxon.where(:name => 'Produkte im Fokus').first
+    slider = Spree::Taxon.where(:id => 2).first
     @slider_products = slider.products.active if slider
 
-    featured = Spree::Taxon.where(:name => 'Beliebte Produkte').first
+    featured = Spree::Taxon.where(:id => 3).first
     @featured_products = featured.products.active if featured
 
     latest = Spree::Taxon.where(:name => 'Latest').first
